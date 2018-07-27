031406062788185cf7c31cdcb40268c4c5047ac2
Simon <s.tretter@gmail.com>
2018-07-27 10:54:58 +0200

    Upstream merge and feature added "noIndexCheck"

--- test/retry.js
+++ test/retry.js
@@ -1,20 +1,21 @@
 /* globals describe, it, beforeEach, afterEach */
 'use strict';
-const MongoClient = require('mongodb').MongoClient;
-const Agenda = require('../index');
+const delay = require('delay');
+const {MongoClient} = require('mongodb');
+const Agenda = require('..');
 
 const mongoHost = process.env.MONGODB_HOST || 'localhost';
 const mongoPort = process.env.MONGODB_PORT || '27017';
-const mongoCfgDb = 'agenda-test';
-const mongoCfg = 'mongodb://' + mongoHost + ':' + mongoPort + '/' + mongoCfgDb;
+const agendaDatabase = 'agenda-test';
+const mongoCfg = 'mongodb://' + mongoHost + ':' + mongoPort + '/' + agendaDatabase;
 
 // Create agenda instances
-let jobs = null;
+let agenda = null;
 let mongoDb = null;
 let mongoClient = null;
 
-const clearJobs = done => {
-  mongoDb.collection('agendaJobs').deleteMany({}, done);
+const clearJobs = () => {
+  return mongoDb.collection('agendaJobs').deleteMany({});
 };
 
 const jobType = 'do work';
@@ -22,45 +23,44 @@ const jobProcessor = () => {};
 
 describe('Retry', () => {
   beforeEach(done => {
-    jobs = new Agenda({
+    agenda = new Agenda({
       db: {
-        database: mongoCfgDb,
         address: mongoCfg
       }
     }, err => {
       if (err) {
         done(err);
       }
-      MongoClient.connect(mongoCfg, (error, client) => {
+      MongoClient.connect(mongoCfg, async(error, client) => {
         mongoClient = client;
-        mongoDb = client.db(mongoCfgDb);
-        clearJobs(() => {
-          jobs.define('someJob', jobProcessor);
-          jobs.define('send email', jobProcessor);
-          jobs.define('some job', jobProcessor);
-          jobs.define(jobType, jobProcessor);
-          done();
-        });
+        mongoDb = client.db(agendaDatabase);
+
+        await delay(50);
+        await clearJobs();
+
+        agenda.define('someJob', jobProcessor);
+        agenda.define('send email', jobProcessor);
+        agenda.define('some job', jobProcessor);
+        agenda.define(jobType, jobProcessor);
+
+        done();
       });
     });
   });
 
-  afterEach(done => {
-    setTimeout(() => {
-      jobs.stop(() => {
-        clearJobs(() => {
-          mongoClient.close(() => {
-            if (jobs._db) jobs._db.close(done);
-            else done();
-          });
-        });
-      });
-    }, 50);
+  afterEach(async() => {
+    await delay(50);
+    await agenda.stop();
+    await clearJobs();
+    await mongoClient.close();
+    await agenda._db.close();
   });
 
-  it('should retry a job', done => {
+  it('should retry a job', async() => {
     let shouldFail = true;
-    jobs.define('a job', (job, done) => {
+
+    agenda.processEvery(100); // Shave 5s off test runtime :grin:
+    agenda.define('a job', (job, done) => {
       if (shouldFail) {
         shouldFail = false;
         return done(new Error('test failure'));
@@ -68,19 +68,20 @@ describe('Retry', () => {
       done();
     });
 
-    jobs.on('fail:a job', (err, job) => {
+    agenda.on('fail:a job', (err, job) => {
       if (err) {
         // Do nothing as this is expected to fail.
       }
       job.schedule('now').save();
     });
 
-    jobs.on('success:a job', () => {
-      done();
-    });
+    const successPromise = new Promise(resolve =>
+      agenda.on('success:a job', resolve)
+    );
 
-    jobs.now('a job');
+    await agenda.now('a job');
 
-    jobs.start();
+    await agenda.start();
+    await successPromise;
   });
 });
